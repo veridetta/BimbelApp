6dd777fe89fbd4470f58abe535cf32fcaf4d40c2
Vr <veridetta@gmail.com>
2022-05-12 13:52:03 +0700

    Revisi login

--- app/src/main/java/com/inc/vr/corps/bimbelapp/ui/nologin/LoginActivity.java
+++ app/src/main/java/com/inc/vr/corps/bimbelapp/ui/nologin/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        getSupportActionBar().hide();
         initView();
         btnClick();
     }
